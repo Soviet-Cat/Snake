6b43dff17fecba044f6b1c170f641495e45e8bd7
Soviet Cat <161828581+Soviet-Cat@users.noreply.github.com>
2025-02-01 16:38:37 +1000

    SDL2 and Basic Window and Loop

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <windows.h>
+#include "libs/SDL2/include/SDL.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    // setup window
+    SDL_Window* window = SDL_CreateWindow(
+        "Snake",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        640, 480,
+        SDL_WINDOW_SHOWN
+        );
+
+    bool running = true;
+    SDL_Event event;
+    // main loop
+    while (running)
+    {
+        // events
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                running = false;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
